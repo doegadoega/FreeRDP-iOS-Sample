2bf96cb8d887cbd61ae91121775f2a0f311b904f
hiroshi egami <doegadoega@hotmail.com>
2025-06-02 01:51:40 +0900

    RDPの処理を修正

--- iOSApp/MyRDPApp/RDP/FreeRDPBridge.h
+++ iOSApp/MyRDPApp/RDP/FreeRDPBridge.h
@@ -7,86 +7,115 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
 #import <CoreGraphics/CoreGraphics.h>
-#import <CoreText/CoreText.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 /// FreeRDPのネイティブライブラリとiOSアプリの間のブリッジングクラス
 @interface FreeRDPBridge : NSObject
 
-/// 初期化（シングルトン）
+/**
+ * シングルトンインスタンスを取得
+ */
 + (instancetype)sharedInstance;
 
-/// RDPサーバーへの接続
-/// @param host ホスト名またはIPアドレス
-/// @param port ポート番号（通常は3389）
-/// @param username ユーザー名
-/// @param password パスワード
-/// @param domain ドメイン名（オプション）
-/// @return 接続プロセスが開始されたかどうか
+#pragma mark - Connection Management
+
+/**
+ * RDPサーバーに接続
+ * @param host ホスト名またはIPアドレス
+ * @param port ポート番号（通常は3389）
+ * @param username ユーザー名
+ * @param password パスワード
+ * @param domain ドメイン名（オプション、nilまたは空文字列可）
+ * @return 接続プロセスが開始されたかどうか
+ */
 - (BOOL)connectToHost:(NSString *)host
                  port:(int32_t)port
              username:(NSString *)username
              password:(NSString *)password
-               domain:(NSString *)domain;
+               domain:(nullable NSString *)domain;
 
-/// 接続を切断
+/**
+ * 接続を切断
+ */
 - (void)disconnect;
 
-/// マウスイベントの送信
-/// @param point 画面上の座標
-/// @param isDown ボタンが押されているかどうか
-/// @param button ボタン（1=左、2=右、3=中）
+#pragma mark - Callback Setters
+
+/**
+ * 接続状態が変更されたときのコールバックを設定
+ * @param block 接続状態が変更されたときに呼ばれるブロック。引数はYESなら接続、NOなら切断。
+ */
+- (void)setOnConnectionStateChanged:(nullable void (^)(BOOL connected))block;
+
+/**
+ * エラーが発生したときのコールバックを設定
+ * @param block エラーが発生したときに呼ばれるブロック。引数はエラーメッセージ。
+ */
+- (void)setOnError:(nullable void (^)(NSString *errorMessage))block;
+
+/**
+ * 画面が更新されたときのコールバックを設定
+ * @param block 画面が更新されたときに呼ばれるブロック。引数は更新された画像。
+ */
+- (void)setOnScreenUpdate:(nullable void (^)(CGImageRef image))block;
+
+#pragma mark - Input Handling
+
+/**
+ * マウスイベントを送信
+ * @param point マウス座標
+ * @param isDown ボタンが押されているかどうか
+ * @param button ボタン番号（1: 左、2: 右、3: 中央）
+ */
 - (void)sendMouseEvent:(CGPoint)point isDown:(BOOL)isDown button:(int32_t)button;
 
-/// キーイベントの送信
-/// @param keyCode キーコード
-/// @param isDown キーが押されているかどうか
+/**
+ * キーイベントを送信
+ * @param keyCode キーコード
+ * @param isDown キーが押されているかどうか
+ */
 - (void)sendKeyEvent:(int32_t)keyCode isDown:(BOOL)isDown;
 
-/// スクロールイベントの送信
-/// @param point 画面上の座標
-/// @param delta スクロール量
+/**
+ * スクロールイベントを送信
+ * @param point マウス座標
+ * @param delta スクロール量
+ */
 - (void)sendScrollEvent:(CGPoint)point delta:(CGFloat)delta;
 
-/// 画面サイズの設定
-/// @param size 画面サイズ
+#pragma mark - Configuration
+
+/**
+ * 画面サイズを設定
+ * @param size 画面サイズ
+ */
 - (void)setScreenSize:(CGSize)size;
 
-/// 色深度の設定
-/// @param colorDepth 色深度（通常は16または32）
+/**
+ * 色深度を設定
+ * @param colorDepth 色深度（通常は16または32）
+ */
 - (void)setColorDepth:(int32_t)colorDepth;
 
-/// 圧縮の有効/無効設定
-/// @param enabled 圧縮を有効にするかどうか
+/**
+ * 圧縮を有効/無効にする
+ * @param enabled 圧縮を有効にするかどうか
+ */
 - (void)setCompressionEnabled:(BOOL)enabled;
 
-/// セキュリティレベルの設定
-/// @param level セキュリティレベル（0-3）
+/**
+ * セキュリティレベルを設定
+ * @param level セキュリティレベル
+ */
 - (void)setSecurityLevel:(int32_t)level;
 
-/// デバッグログの有効/無効設定
-/// @param enabled デバッグログを有効にするかどうか
+/**
+ * デバッグログを有効/無効にする
+ * @param enabled デバッグログを有効にするかどうか
+ */
 - (void)enableDebugLogging:(BOOL)enabled;
-
-/// 高度なRDP接続テスト
-- (void)testAdvancedRDPConnection;
-
-#pragma mark - コールバック設定
-
-/// 接続状態変更通知ブロック
-/// @param block 状態変更時に呼び出されるブロック
-- (void)setOnConnectionStateChanged:(void (^)(BOOL isConnected))block;
-
-/// エラー通知ブロック
-/// @param block エラー発生時に呼び出されるブロック
-- (void)setOnError:(void (^)(NSString *errorMessage))block;
-
-/// 画面更新通知ブロック
-/// @param block 画面更新時に呼び出されるブロック
-- (void)setOnScreenUpdate:(void (^)(CGImageRef image))block;
 
 @end
 
